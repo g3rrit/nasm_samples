da5f9e671a5d9bd74acae5dca66a67bcb1bf69f4
pearisgreen <grproessl@web.de>
2018-01-16 10:16:13 +0100

    update

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,9 @@
 ; link:         gcc main.o -o programm
 
 extern print.start
+extern print.error
+extern print.rax
+extern print.stack
 
 global main
 
@@ -12,6 +15,20 @@ main:
             mov     rbp, rsp
 
             call    print.start
+
+            mov     rax, 0
+            sub     rax, 1
+            call    print.rax
+
+            push    qword 6
+            push    qword 6
+            push    qword 6
+            push    qword 6
+            push    qword 26
+            call    print.stack
+            add     rsp , 4
+    
+            call    print.error
     
             pop     rbp
             ret
--- src/print.asm
+++ src/print.asm
@@ -1,7 +1,11 @@
 
 extern printf
+extern _printf
 
 global print.start
+global print.rax
+global print.error
+global print.stack
 
 section .text
 
@@ -12,13 +16,46 @@ print.start:
             call    printf   
             ret
 
-print.eax:
-            mov     rdi, str_print_start
+print.rax:
+            mov     rdi, str_print_rax
+            mov     rsi, rax
+            xor     rax, rax
+            call    printf
+            ret
+
+print.stack:
+            ; qword [rsp+8] holds repetition values
+            ; qword [rsp+8*i] holds byte*
+    
+            cmp     qword [rsp+8], 1
+            jl      print.stack.end 
+
+            print.stack.label1:
+            sub     qword [rsp+8], 1
+            mov     rdi, str_stack_s
+            mov     rsi, qword [rsp+8]
+            mov     rax, qword [rsp+8]
+            mov     rdx, qword [rsp+8*rax]
+            xor     rax, rax
+            call    printf
+            cmp     qword [rsp+8], 0
+            jg      print.stack.label1
+            
+            print.stack.end:
+   
+            ret
+
+print.error:
+            mov     rdi, str_print_error
             xor     rax, rax
             call    printf
             ret
 
 section .data
+
+str_stack_s:        db "[rsp + %i*8]: %#8x", 10, 0
+
+str_print_error:    db "error", 10, 0
 
 str_print_rax:      db "rax: %#8x", 10, 0
 
